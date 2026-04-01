15f94ff634b9185e6a4830ad12ef982174da49f2
tracer-ics2017 <tracer@njuics.org>
2026-04-01 23:24:04 +0800

    >  compile 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:24:04 up 2 days, 12:32,  3 users,  load average: 0.02, 0.05, 0.00 bdb936f3225faf646d3b2312d26c76d070997851

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -51,8 +51,8 @@ make_group(gp2,
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    EMPTY, EMPTY, EX(not), EMPTY,
-    EX(test), EMPTY, EMPTY, EMPTY)
+    EX(test), EMPTY, EX(not), EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY)
 
   /* 0xfe */
 make_group(gp4,
@@ -133,7 +133,7 @@ opcode_entry opcode_table [512] = {
   /* 0xe8 */	IDEX(J, call), IDEX(J, jmp), EMPTY, IDEXW(J, jmp, 1),
   /* 0xec */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xf4 */	IDEXW(E, gp3, 1), IDEX(E, gp3), IDEXW(test_I, gp3, 1), IDEX(test_I, gp3),
+  /* 0xf4 */	IDEXW(E, gp3, 1), IDEX(E, gp3), IDEXW(I2E, gp3, 1), IDEX(I2E, gp3),
   /* 0xf8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xfc */	IDEXW(E, gp4, 1), IDEX(E, gp5), IDEXW(E, gp4, 1), IDEX(E, gp5),
 
