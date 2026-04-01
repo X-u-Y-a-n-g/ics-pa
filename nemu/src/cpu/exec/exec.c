4aedc140836d299914d16be0dbce0f0b801b1d63
tracer-ics2017 <tracer@njuics.org>
2026-04-02 00:08:43 +0800

    >  compile 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:08:43 up 2 days, 13:17,  3 users,  load average: 0.02, 0.04, 0.00 74638dffd0004aff4ec484a0e778883cd1c5a88c

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -185,7 +185,7 @@ opcode_entry opcode_table [512] = {
   /* 0xac */	EMPTY, EMPTY, EMPTY, IDEX(E2G, imul2),
   /* 0xb0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb4 */	EMPTY, EMPTY, IDEXW(E2G, movzx, 1), IDEXW(E2G, movzx, 2),
-  /* 0xb8 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0xb8 */	EMPTY, EMPTY, IDEXW(E2G, movsx, 1), EMPTY,
   /* 0xbc */	EMPTY, EMPTY, EMPTY, IDEXW(E2G, movsx, 2),
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc4 */	EMPTY, EMPTY, EMPTY, EMPTY,
