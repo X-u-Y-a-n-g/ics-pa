2da89d5c9a0baec10a54cc7ad9901b30d8970b70
tracer-ics2017 <tracer@njuics.org>
2026-04-02 01:15:26 +0800

    >  run 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:15:26 up 2 days, 14:23,  4 users,  load average: 0.01, 0.05, 0.02 70ce8c2c558e545b3a64663b476f2319592ab5c1

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,7 +9,8 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  return 0;
+  // return 0;
+  return inl(RTC_PORT) - boot_time;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
@@ -22,9 +23,22 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  // int i;
+  // for (i = 0; i < _screen.width * _screen.height; i++) {
+  //   fb[i] = i;
+  // }
+  if (w <= 0 || h <= 0) return;
+  if (x < 0) { pixels -= x; w += x; x = 0; }
+  if (y < 0) { pixels -= y * w; h += y; y = 0; }
+  if (x + w > _screen.width)  w = _screen.width - x;
+  if (y + h > _screen.height) h = _screen.height - y;
+  if (w <= 0 || h <= 0) return;
+
+  int row;
+  for (row = 0; row < h; row++) {
+    memcpy(fb + (y + row) * _screen.width + x,
+           pixels + row * w,
+           w * sizeof(uint32_t));
   }
 }
 
@@ -32,5 +46,8 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  return _KEY_NONE;
+  // return _KEY_NONE;
+  uint8_t status = inb(0x64);
+  if ((status & 0x1) == 0) return _KEY_NONE;
+  return inl(0x60);
 }
