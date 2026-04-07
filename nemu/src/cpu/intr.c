160636f358d9be06bcaeba7417ffa9f9fa24acf8
tracer-ics2017 <tracer@njuics.org>
2026-04-07 23:09:20 +0800

    >  compile 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:09:20 up 4 days,  4:40,  4 users,  load average: 0.05, 0.03, 0.00 34065a053e3676b6829ecb42a6bdd3bbdf73df2a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,18 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  // TODO();
+  rtlreg_t esp = reg_l(R_ESP);
+  esp -= 4; vaddr_write(esp, 4, cpu.eflags);
+  esp -= 4; vaddr_write(esp, 4, cpu.cs);
+  esp -= 4; vaddr_write(esp, 4, ret_addr);
+  reg_l(R_ESP) = esp;
+
+  vaddr_t gate_addr = cpu.idtr.base + NO * 8;
+  uint32_t low = vaddr_read(gate_addr, 4);
+  uint32_t high = vaddr_read(gate_addr + 4, 4);
+  assert((high >> 15) & 0x1);
+  cpu.eip = (high & 0xFFFF0000) | (low & 0xFFFF);
 }
 
 void dev_raise_intr() {
