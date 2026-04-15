a08dcc834c0b90192231035f848c2b9aee6b374e
tracer-ics2017 <tracer@njuics.org>
2026-04-15 09:54:11 +0800

    >  run 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:54:11 up 7 days, 19:54,  5 users,  load average: 0.54, 0.15, 0.06 d080c0e06e71b9409583cdc19e13976e60b10229

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,15 +3,16 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-void ramdisk_read(void *buf, off_t offset, size_t len);
-size_t get_ramdisk_size();
+// void ramdisk_read(void *buf, off_t offset, size_t len);
+// size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
   // size_t size = get_ramdisk_size();
   // ramdisk_read(DEFAULT_ENTRY, 0, size);
   // return (uintptr_t)DEFAULT_ENTRY;
-  const char *fname = filename ? filename : "/bin/init";
+  (void)as;
+  const char *fname = (filename == NULL ? "/bin/text" : filename);
   int fd = fs_open(fname, 0, 0);
   size_t size = fs_filesz(fd);
   fs_read(fd, DEFAULT_ENTRY, size);
