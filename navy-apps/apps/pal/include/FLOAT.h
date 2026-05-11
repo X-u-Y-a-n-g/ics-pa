3fca0daf2e110a950a75e0b7fb2a543ff7291d58
tracer-ics2017 <tracer@njuics.org>
2026-05-11 11:36:13 +0800

    >  compile 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:36:13 up  6:45,  2 users,  load average: 0.46, 0.16, 0.05 4d22f3e9fcb0ab45b8f739417efe43811589dff4

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -2,27 +2,36 @@
 #define __FLOAT_H__
 
 #include "assert.h"
+#include <stdint.h>
 
 typedef int FLOAT;
 
+#define FLOAT_FRAC_BITS 16
+#define FLOAT_SCALE (1 << FLOAT_FRAC_BITS)
+
 static inline int F2int(FLOAT a) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  return a / FLOAT_SCALE;
 }
 
 static inline FLOAT int2F(int a) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  return (FLOAT)((int64_t)a * FLOAT_SCALE);
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  return (FLOAT)((int64_t)a * b);
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  assert(b != 0);
+  return a / b;
 }
 
 FLOAT f2F(float);
--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,13 +3,16 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  return (FLOAT)(((int64_t)a * b) / FLOAT_SCALE);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  assert(b != 0);
+  return (FLOAT)(((int64_t)a * FLOAT_SCALE) / b);
 }
 
 FLOAT f2F(float a) {
@@ -23,13 +26,33 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+
+  uint32_t bits = *(uint32_t *)&a;
+  uint32_t sign = bits >> 31;
+  uint32_t exp = (bits >> 23) & 0xff;
+  uint32_t frac = bits & 0x7fffff;
+
+  assert(exp != 0xff);
+
+  if (exp == 0) {
+    return 0;
+  }
+
+  uint64_t mant = (1u << 23) | frac;
+  int shift = (int)exp - 127 - 7;
+  uint64_t val = (shift >= 0) ? (mant << shift) : (mant >> -shift);
+
+  assert((!sign && val <= INT32_MAX) || (sign && val <= (uint64_t)INT32_MAX + 1));
+  return sign ? -(FLOAT)val : (FLOAT)val;
 }
 
 FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+  // assert(0);
+  // return 0;
+  assert(a != INT32_MIN);
+  return a < 0 ? -a : a;
 }
 
 /* Functions below are already implemented */
