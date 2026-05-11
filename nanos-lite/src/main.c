fb4d8480e58693b43ae5c798086dcb303429dbc6
tracer-ics2017 <tracer@njuics.org>
2026-05-11 11:49:16 +0800

    >  run 2313721 icspa Linux icspa 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:49:16 up  6:58,  2 users,  load average: 0.43, 0.19, 0.09 4c68c6b99533db04b861046f2719ab55e15e4e12

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -34,7 +34,7 @@ int main() {
   // uint32_t entry = loader(NULL, NULL);
   // ((void (*)(void))entry)();
   load_prog("/bin/pal");
-  load_prog("/bin/hello");
+  // load_prog("/bin/hello");
   _trap();
 
   panic("Should not reach here");
